fb73e192552c69802aa763a42b73faff0c3242a6
dorinesinenco <dorin.e.art@gmail.com>
2018-10-15 20:35:46 +0300

    Add files via upload

---
+++ CodingOOP/src/ariborn/AirPlane.java
@@ -0,0 +1,31 @@
+package ariborn;
+
+
+// BASE CLASS / SUPER
+public class AirPlane {
+	private int fuel; // bak 
+	byte seats; // mesta
+	int distance; // probeg
+	
+	// default
+	public AirPlane(){
+		
+	}
+	
+	public AirPlane(int fuel, int seats){
+		if(fuel>0&&seats>0){
+			this.distance = 0;
+			this.fuel     = fuel;
+			this.seats    = (byte)seats;
+		}else{
+			System.err.println( "WRONG!" );
+		}
+	}
+	
+	 public String toString(){
+		 return "\nPLANE \nTANK " + this.fuel
+		 + "\nSEATS " + this.seats
+		 + "\nDISTANCE " + this.distance;
+	 }
+
+}
---
+++ CodingOOP/src/main/CodingApp.java
@@ -0,0 +1,21 @@
+package main;
+
+import ariborn.AirPlane;
+
+public class CodingApp {
+
+	
+	
+	
+	public static void main(String[] args) {
+		
+		AirPlane plane1 = new AirPlane(1000,100);
+		
+		System.out.println(
+			plane1 // .toString() 
+		);
+		
+		
+	}
+
+}
